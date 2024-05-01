d22d4f407d822ce8f620d0d571afc0aa07837fe2
tubisaffo <tubisaffo@gmail.com>
2024-05-01 19:46:47 +0300

    adding states and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  const addToCart = () => {
+    setIsInCart(!isInCart); // Toggle the previous state
+  };
+
+  const itemClass = isInCart ? "in-cart" : "item";
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart";
+  const buttonColor = isInCart ? "magenta" : "yellow";
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button
+        className="add"
+        style={{ backgroundColor: buttonColor }}
+        onClick={addToCart}
+      >
+        {buttonText}
+      </button>
     </li>
   );
 }
